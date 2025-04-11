0f6f6f89155519a0963bfaa73c883d6184aa95fe
shg-jmathews <jmathews@sledgehammergames.com>
2025-04-11 11:40:24 -0700

    player done

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     while True:
@@ -21,9 +24,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for i in drawable:
+            i.draw(screen)
         pygame.display.flip()
 
         #limits fps to 60
--- player.py
+++ player.py
@@ -22,6 +22,10 @@ class Player(CircleShape):
             self.rotate(reverse_dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(reverse_dt)
 
     # in the player class
     def triangle(self):
@@ -34,3 +38,7 @@ class Player(CircleShape):
     
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
